902097d0663c4e9cfc56ec1eeb53472f396e6978
ihassanarif29 <ihassanarif29@gmail.com>
2026-02-21 10:30:28 +0500

    explain variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144557
+let accountEmail = "hassan@google.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+
+
+// this is not allowed
+// accountId = 39
+
+//console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
