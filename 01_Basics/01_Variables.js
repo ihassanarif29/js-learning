76118d9d52f8068a7f2447500c3678a78ad7c391
ihassanarif29 <ihassanarif29@gmail.com>
2026-02-21 10:56:52 +0500

    explain data types

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -7,6 +7,12 @@ accountCity = "Lahore"
 // this is not allowed
 // accountId = 39
 
+
+/*
+prefer not to use var as a variable in java script
+because of issue in block scope and functional scopt
+*/
+
 //console.log(accountId)
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS Code as a newer JS version
+
+// alert( 3+5) // we are using nodejs, not browser that is why this function is working
+
+let name = "Hassan" // String data type
+let age = 29 // int data type
+let isLoggedIn = false // Boolean data type
+
+
+// poremitive data types
+// number => 2 to power53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined => not define yet
+// symbol => unique symbol
+
+
+
+// object
+
+// get type of varialbe 
+console.log(typeof "Hassan");
