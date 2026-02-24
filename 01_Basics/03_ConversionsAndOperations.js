fe8378f3ff190621d48c259bfee2205a1add1048
ihassanarif29 <ihassanarif29@gmail.com>
2026-02-24 22:40:12 +0500

    Conversions And Operations

---
+++ 01_Basics/03_ConversionsAndOperations.js
@@ -0,0 +1,38 @@
+
+
+// let scrore = "33"
+// console.log(typeof scrore);
+
+
+// let valueInNumber = Number(scrore)
+// console.log(typeof valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+// let scrore = "33abc"
+// console.log(typeof scrore);
+
+
+// let valueInNumber = Number(scrore)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false, "anything" => true
+
+
+let number = 29
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
