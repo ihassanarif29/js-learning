78d3ea7ebafb9f2b6b7b329a7bb0e69f5a207a6a
ihassanarif29 <ihassanarif29@gmail.com>
2026-02-25 02:57:35 +0500

    Operations

--- 01_Basics/03_ConversionsAndOperations.js
+++ 01_Basics/03_ConversionsAndOperations.js
@@ -34,5 +34,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let number = 29
 let stringNumber = String(number)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+// ************************* Operations ************************
+
+let value = 3
+let negativeValue = -value
+
+//console.log(negativeValue)
+
+// add two numbers
+//console.log(2+2)
+
+// subtract two numbers
+//console.log(5-2)
+
+// multiply two numbers
+//console.log(3*2)
+
+// power of a number
+//console.log(2**5)
+
+// divide two nubers
+//console.log(3/4)
+
+// remainder
+//console.log(2%3)
+
+
+let str1 = "Hi "
+let str2 = "Hassan"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
