aa39608c5fe962fc45b4598cac80a8523a269d9d
ihassanarif29 <ihassanarif29@gmail.com>
2026-04-08 04:56:14 +0500

    add stack and heap example

--- 01_Basics/05_Datatypes.js
+++ 01_Basics/05_Datatypes.js
@@ -15,7 +15,7 @@ let userEmail; // undefined value
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // Non-Primitive Data Type (Refrence Type)
 // Call by refrence
@@ -32,4 +32,35 @@ const MyFunction = function () {
     console.log("Hello this is my function")
 }
 
-console.log(typeof scoreValue);
+//console.log(typeof scoreValue);
+
+
+
+//==============================================
+// Stack Memory, Heap Memory
+//==============================================
+// Stack memory is used where primitive variable is used
+// Heap memoty is used where non-primotive variable is used
+
+// Stack memoty example
+let myVariable = "Hassan"
+let myAnotherVariable = myVariable
+myAnotherVariable = "Another Name"
+console.log(myAnotherVariable)
+console.log(myVariable)
+
+
+
+let userOne = {
+    name: "Hassan",
+    email: "hassan@gmail.com",
+    address: "h1 jinnah street"
+}
+
+let userTwo = userOne
+userTwo.email = "newhassan@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
