c6324ad255cb2f06016bf21cac54215822140f0f
ihassanarif29 <ihassanarif29@gmail.com>
2026-04-11 00:58:53 +0500

    Strings

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,40 @@
+const name = "Hassan"
+const repoCount = 50
+
+//console.log(name + " Value " + repoCount)
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Super Mario')
+
+//console.log(gameName[0])
+
+//console.log(gameName.__proto__)
+
+//console.log(gameName.length)
+
+//console.log(gameName.toUpperCase())
+
+//console.log(gameName.charAt(3))
+
+//console.log(gameName.indexOf("M"))
+
+// const newSubString = gameName.substring(5, 9)
+// console.log(newSubString)
+
+// const newSlicedString = gameName.slice(0, 4)
+// console.log(newSlicedString)
+
+// String Trim Example
+// const newStringOne = "   Hassan Arif   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+
+// const url = "https://www.mysite.com/hassan%20arif"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('hassan'))
+
+// Split string based upon given string
+const myFriends = "hassan-noman-rehmat-umair-mohsin"
+console.log(myFriends.split('-'))
