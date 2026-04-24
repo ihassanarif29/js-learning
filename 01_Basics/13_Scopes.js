97c20686ca1aff5c7a627e498be8da92fad21712
ihassanarif29 <ihassanarif29@gmail.com>
2026-04-25 01:50:46 +0500

    Nested Scope Hoisting

--- 01_Basics/13_Scopes.js
+++ 01_Basics/13_Scopes.js
@@ -7,6 +7,33 @@ if (true) {
   var c = 30;
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+//====================== Nested Scope =========================
+
+function one() {
+  const username = "hassan";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //console.log(website);
+  two();
+}
+
+//one();
+
+//====================== Hoisting =========================
+
+console.log(addOne(5));
+function addOne(number) {
+  return number + 1;
+}
+
+addTwo(5);
+const addTwo = function (number) {
+  return number + 2;
+};
