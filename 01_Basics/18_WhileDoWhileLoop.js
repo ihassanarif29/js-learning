f2d85fb3af972b62bd58ff750a2f710cb6f37d15
Hassan Arif <ihassanarif29@gmail.com>
2026-05-17 04:27:19 +0500

    Add While and Do While Loop Exmple

---
+++ 01_Basics/18_WhileDoWhileLoop.js
@@ -0,0 +1,13 @@
+// While Loop
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// Do While Loop
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
